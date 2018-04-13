9fbfb164b7abf794afb99c55ed5b0e79502423c9
zhangliangk <zhangliangk@yutong.com>
2018-04-13 09:46:44 +0800

    create project

---
+++ src/main/java/com/loktar/xyk/XykApplication.java
@@ -0,0 +1,12 @@
+package com.loktar.xyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XykApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XykApplication.class, args);
+    }
+}
---
+++ src/test/java/com/loktar/xyk/XykApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loktar.xyk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XykApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
